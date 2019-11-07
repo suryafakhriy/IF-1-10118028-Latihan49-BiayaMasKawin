a67480dfb78fa126a178608da19881c4dacdb4c4
suryafakhriy <55834014+suryafakhriy@users.noreply.github.com>
2019-11-07 22:49:30 +0700

    Add files via upload

---
+++ IF-1-10118028-Latihan49-BiayaMasKawin/IF-1-10118028-Latihan47-NilaiMahasiswa/src/pkgif/pkg1/pkg10118028/latihan47/nilaimahasiswa/IF110118028Latihan47NilaiMahasiswa.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgif.pkg1.pkg10118028.latihan47.nilaimahasiswa;
+
+/**
+ *
+ * @author User
+ * NAMA      : Surya Fakhriy Hasbi
+   KELAS     : IF-01 2018
+   NIM       : 10118028
+   DESKRIPSI : Nilai Mahasiswa
+ */
+public class IF110118028Latihan47NilaiMahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NilaiMhs mhs = new NilaiMhs(75,45,34);
+        NilaiMhs mhs1 = new NilaiMhs(60, 80, 75);
+        NilaiMhs mhs2 = new NilaiMhs(30, 80, 40);
+        
+        
+    }
+    
+}
---
+++ IF-1-10118028-Latihan49-BiayaMasKawin/src/pkgif/pkg1/pkg10118028/latihan49/biayamaskawin/IF110118028Latihan49BiayaMasKawin.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgif.pkg1.pkg10118028.latihan49.biayamaskawin;
+
+/**
+ *
+ * @author User
+ * NAMA      : Surya Fakhriy Hasbi
+   KELAS     : IF-01 2018
+   NIM       : 10118028
+   DESKRIPSI : Biaya Emas Kawin
+
+ */
+public class IF110118028Latihan49BiayaMasKawin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        EmasKawin mkawin = new EmasKawin();
+        mkawin.tampilMahar();
+    }
+    
+}
